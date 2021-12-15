5921ce5937f057822ba8287e38bbe309371f2382
TJZYMR <tatva.joshi@zymr.com>
2021-12-15 13:03:46 +0530

    After importng Data on main file

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"y/M"
-	"y/Structs_for_patterns"
-	"y/utils"
+	"fmt"
+	"path/filepath"
+	"y/Generic"
 )
 
 func main() {
-	t := [][]int{{1, 697}, {2, 703}, {3, 692}, {4, 710}, {5, 736}, {6, 745}, {7, 736}, {8, 756}, {9, 785}, {10, 811}, {11, 795}, {12, 785}, {13, 787}, {14, 801}, {15, 838}, {16, 826}, {17, 801}, {18, 778}, {19, 778}, {20, 786}, {21, 781}, {22, 764}, {23, 773}, {24, 789}, {25, 773}, {26, 775}, {27, 792}, {28, 779}, {29, 780}, {30, 798}, {31, 807}, {32, 806}, {33, 802}, {34, 794}, {35, 779}, {36, 752}, {37, 739}, {38, 725}, {39, 728}, {40, 738}, {41, 731}, {42, 743}, {43, 745}, {44, 772}, {45, 765}, {46, 756}, {47, 755}, {48, 741}, {49, 738}, {50, 751}, {51, 770}, {52, 786}, {53, 783}, {54, 783}, {55, 773}, {56, 768}, {57, 774}, {58, 781}, {59, 796}, {60, 798}, {61, 781}}
-	//var m Patterns
-	p := []string{"Upward", "Downward", "Upward", "Downward"}
-	m := &M.Letterm{t, p}
-	// a, b, c, _ := m.Pattern()
-	a, b, c, _ := Structs_for_patterns.Find(m)
-	utils.Plot1(t, c, a, b)
+	// t := [][]int{{1, 697}, {2, 703}, {3, 692}, {4, 710}, {5, 736}, {6, 745}, {7, 736}, {8, 756}, {9, 785}, {10, 811}, {11, 795}, {12, 785}, {13, 787}, {14, 801}, {15, 838}, {16, 826}, {17, 801}, {18, 778}, {19, 778}, {20, 786}, {21, 781}, {22, 764}, {23, 773}, {24, 789}, {25, 773}, {26, 775}, {27, 792}, {28, 779}, {29, 780}, {30, 798}, {31, 807}, {32, 806}, {33, 802}, {34, 794}, {35, 779}, {36, 752}, {37, 739}, {38, 725}, {39, 728}, {40, 738}, {41, 731}, {42, 743}, {43, 745}, {44, 772}, {45, 765}, {46, 756}, {47, 755}, {48, 741}, {49, 738}, {50, 751}, {51, 770}, {52, 786}, {53, 783}, {54, 783}, {55, 773}, {56, 768}, {57, 774}, {58, 781}, {59, 796}, {60, 798}, {61, 781}}
+	// //var m Patterns
+	// p := []string{"Upward", "Downward", "Upward", "Downward"}
+	// m := &M.Letterm{t, p}
+	// // a, b, c, _ := m.Pattern()
+	// a, b, c, _ := Structs_for_patterns.Find(m)
+	// utils.Plot1(t, c, a, b)
 	//w
 	// p1 := []string{"Downward", "Upward", "Downward", "Upward"}
 	// w := &m_struct.Letterw{t, p1}
@@ -21,4 +21,46 @@ func main() {
 	// a1, b1, c1, _ := m_struct.Find(w)
 	// utils.Plot1(t, c1, a1, b1)
 
+	//1.finding file type
+	//now we have file name and we wamt to find the file type
+	// var filetype string
+	// var filepath string
+
+	path := "/home/tatva.j@ah.zymrinc.com/Desktop/go-graph/Generic/date-values.csv"
+	var data []Generic.Date
+	fileExtension := filepath.Ext(path)
+	fmt.Println(fileExtension)
+	if fileExtension == ".xlsx" {
+		data = Generic.Genericexcel(path, "date-values")
+		fmt.Println("data received:=>", "File Type:=>Excel   ", data)
+	} else if fileExtension == ".csv" {
+		data = Generic.Genericcsv(path)
+		fmt.Println("data received:=>", "File Type:=>csv   ", data)
+
+	} else if fileExtension == ".json" {
+		data = Generic.Genericjson(path)
+		fmt.Println("data received:=>", "File Type:=>json   ", data)
+
+	} else if fileExtension == ".txt" {
+		data = Generic.Genericexcel(path, "date-values")
+		fmt.Println("data received:=>", "File Type:=>txt   ", data)
+
+	} else if fileExtension == ".xml" {
+		data = Generic.Genericexcel(path, "date-values")
+		fmt.Println("data received:=>", "File Type:=>xml   ", data)
+
+	} else if fileExtension == ".html" {
+		data = Generic.Genericexcel(path, "date-values")
+		fmt.Println("data received:=>", "File Type:=>html   ", data)
+
+	} else if fileExtension == ".xls" {
+		data = Generic.Genericexcel(path, "date-values")
+		fmt.Println("data received:=>", "File Type:=>xls   ", data)
+
+	} else { //here database is the file type
+		data := Generic.Genericdatabase("127.0.0.1:3306", "tatva", "tatva", "zymr@123")
+		fmt.Println("data received:=>", data)
+
+	}
+	fmt.Println(data)
 }
